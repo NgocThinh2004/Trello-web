7b0a59c7184e814174d262af2ab382eb80940679
NgocThinh2004 <nguyenthinhlg2@gmail.com>
2026-03-14 18:52:16 +0700

    Config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
-      <div>Thinhv</div>
+      <div>Thinh</div>
+      <Typography variant='body2' color="text.secondary">Test abcd</Typography>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color="success">Contained</Button>
       <Button variant="outlined">Outlined</Button>
       <AccessAlarmIcon />
       <ThreeDRotation />
@@ -18,6 +20,9 @@ function App() {
       <HomeIcon color="action" />
       <HomeIcon color="disabled" />
       <HomeIcon sx={{ color: pink[500] }} />
+      <Button color="secondary" variant="contained">
+      Save
+      </Button>
     </>
   )
 }
