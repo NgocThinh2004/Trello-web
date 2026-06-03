c27c1003eab33282a45e15d780be784044e28ae8
NgocThinh2004 <nguyenthinhlg2@gmail.com>
2026-06-03 14:45:52 +0700

    implement base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import { pink } from '@mui/material/colors'
-import HomeIcon from '@mui/icons-material/Home'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
-
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
@@ -14,6 +7,7 @@ import Box from '@mui/material/Box'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import { Container } from '@mui/material'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -51,42 +45,38 @@ function ModeSelect() {
     </FormControl>
   )
 }
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
+
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle/>
-      <hr />
-      <div>Thinh</div>
-      <Typography variant='body2' color="text.secondary">Test abcd</Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color="success">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-      <Button color="secondary" variant="contained">
-      Save
-      </Button>
-    </>
+    <Container disableGutters maxWidth={false} sx = {{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor:'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx={{
+        backgroundColor:'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width:'100%',
+        height:(theme) => `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
